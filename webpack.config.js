ea070d339ccd48c594c31b6d085275ce6d4a0c0e
Tasveer Singh <taz@tazsingh.com>
2015-11-07 08:34:58 -0800

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./index.js"
+, output: {
+    path: __dirname + "/build"
+  , publicPath: "/assets/"
+  , filename: "bundle.js"
+  }
+, resolve: {
+    extensions: [
+      ""
+    , ".js"
+    ]
+  }
+, module: {
+    loaders: [
+      {
+        test: /\.js$/
+      , exclude: /node_modules/
+      , loader: "babel-loader"
+      }
+    , {
+        test: /\.less$/
+      , loader: "style-loader!css-loader!less-loader"
+      }
+    , {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/
+      , loader: "url?limit=73728"
+      }
+    , {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/
+      , loader: "file"
+      }
+    , {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/
+      , loader: "file"
+      }
+    , {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/
+      , loader: "url?limit=102400"
+      }
+    , {
+        test: /\.(jpe?g|png)(\?v=\d+\.\d+\.\d+)?$/
+      , loader: "url?limit=102400"
+      }
+    ]
+  }
+, plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(true)
+  ]
+}
