78c943c7ee0be0e98c166a1861e04a2b9c10b72a
Tasveer Singh <taz@tazsingh.com>
2015-11-07 08:45:00 -0800

    Pull in react-router and get a basic page rendering

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+import { Router, Route, Link } from "react-router";
+
+let HomeComponent = (props) => {
+  return (
+    <div>
+      <h1>It works!</h1>
+    </div>
+  );
+}
+
+React.render(
+  <Router>
+    <Route
+      path="/" component={HomeComponent}
+    />
+  </Router>
+, container); // container is defined as an ID existing in the DOM
