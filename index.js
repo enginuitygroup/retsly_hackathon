e9b80d7e5643a3881db18abc61fb3b0b6cf22d25
Tasveer Singh <taz@tazsingh.com>
2015-11-07 21:34:56 -0800

    Add heat maps with contours!!!!

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//import es6promise from "es6-promise";
-//es6promise.polyfill();
 import "babel/polyfill";
 
 import { Router, Route, Link } from "react-router";
@@ -11,6 +9,7 @@ import createBrowserHistory from "history/lib/createBrowserHistory";
 // this sets a global `L` because of Leaflet... :(
 import "mapbox.js";
 L.mapbox.accessToken = "pk.eyJ1IjoidGF6c2luZ2giLCJhIjoiRVNQcGw5OCJ9.GKf__kb42ilFSxwdxkLDWQ";
+import "leaflet.heat";
 
 import "./styles/bootstrap_imports.less";
 
