0b72b42e41dfcd0360048329f4ec34f5d0d609b4
soojeen <soojeen@gmail.com>
2015-11-08 11:20:55 -0800

    add filters for retsly

--- actions/RetslyListingData.js
+++ actions/RetslyListingData.js
@@ -2,10 +2,26 @@ import fetch from "isomorphic-fetch";
 import {Action} from "griffin.js";
 
 export default class RetslyListingData extends Action {
-  constructor(listingId) {
+  constructor(listingId, filters) {
     super();
 
     let params = `access_token=${process.env.RETSLY_TOKEN}`;
+
+    if(filters) {
+      if(filters.beds) {
+        let beds = `&bedrooms=${filters.beds}`;
+        params.concat(beds);
+      }
+      if(filters.baths) {
+        let baths = `&baths=${filters.baths}`;
+        params.concat(baths);
+      }
+      if(filters.price) {
+        let price = `&price[gt]${filter.price}`;
+        params.concat(price)
+      }
+    }
+
     fetch(`https://rets.io/api/v1/test_sf/listings/${listingId}?${params}`, {cors: true}).then(
       this.handleSuccess.bind(this)
     , this.handleFailure.bind(this)
