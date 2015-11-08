c0717d7cba20f9fcfb7b5190b0941fa006395f2e
soojeen <soojeen@gmail.com>
2015-11-08 11:28:18 -0800

    move filtering to box store

--- actions/RetslyListingData.js
+++ actions/RetslyListingData.js
@@ -7,21 +7,6 @@ export default class RetslyListingData extends Action {
 
     let params = `access_token=${process.env.RETSLY_TOKEN}`;
 
-    if(filters) {
-      if(filters.beds) {
-        let beds = `&bedrooms=${filters.beds}`;
-        params.concat(beds);
-      }
-      if(filters.baths) {
-        let baths = `&baths=${filters.baths}`;
-        params.concat(baths);
-      }
-      if(filters.price) {
-        let price = `&price[gt]${filter.price.slice(0, -1)}`;
-        params.concat(price)
-      }
-    }
-
     fetch(`https://rets.io/api/v1/test_sf/listings/${listingId}?${params}`, {cors: true}).then(
       this.handleSuccess.bind(this)
     , this.handleFailure.bind(this)
--- actions/RetslyListingsByBox.js
+++ actions/RetslyListingsByBox.js
@@ -5,7 +5,26 @@ export default class RetslyListingData extends Action {
   constructor(northWest, southEast, filteringOptions) {
     super();
 
-    let params = `access_token=${process.env.RETSLY_TOKEN}&box=${northWest.lng},${northWest.lat},${southEast.lng},${southEast.lat}&limit=25&sortBy=price&`;
+    let params = `access_token=${process.env.RETSLY_TOKEN}&limit=25&sortBy=price&`;
+    let bbox = `&box=${northWest.lng},${northWest.lat},${southEast.lng},${southEast.lat}`;
+
+    params.concat(bbox);
+
+    if(filteringOptions) {
+      if(filteringOptions.beds) {
+        let beds = `&bedrooms=${filteringOptions.beds}`;
+        params.concat(beds);
+      }
+      if(filteringOptions.baths) {
+        let baths = `&baths=${filteringOptions.baths}`;
+        params.concat(baths);
+      }
+      if(filteringOptions.price) {
+        let price = `&price[gt]${filteringOptions.price.slice(0, -1)}`;
+        params.concat(price)
+      }
+    }
+
     fetch(`https://rets.io/api/v1/test_sf/listings?${params}`, {cors: true}).then(
       this.handleSuccess.bind(this)
     , this.handleFailure.bind(this)
