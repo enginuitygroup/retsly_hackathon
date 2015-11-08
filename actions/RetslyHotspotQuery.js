27e055382767359c4766c6e6cfbd02ab27c1560d
Adam Jasiura <adam.jasiura@gmail.com>
2015-11-08 12:41:09 -0800

    Fix bug with url-encoding passed-in addresses

--- actions/RetslyHotspotQuery.js
+++ actions/RetslyHotspotQuery.js
@@ -9,7 +9,7 @@ export default class RetslyHotspotQuery extends Action {
 
     let oldestDate = Moment().subtract(1, "year").format();
 
-    let params = `access_token=${process.env.RETSLY_TOKEN}&near=${address}&radius=1km&documentDate.gt=${oldestDate}`;
+    let params = `access_token=${process.env.RETSLY_TOKEN}&near=${encodeURIComponent(address)}&radius=1km&documentDate.gt=${oldestDate}`;
     fetch(`https://rets.io/api/v1/test_sf/transactions/?${params}`, {cors: true}).then(
       this.handleSuccess.bind(this)
     , this.handleFailure.bind(this)
