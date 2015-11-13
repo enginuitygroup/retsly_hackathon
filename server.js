52bea4dac12465f96a401347c3c1e386d8770f47
Tasveer Singh <taz@tazsingh.com>
2015-11-13 11:48:38 -0500

    Add basic auth

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 import express from "express";
+import basicAuth from "basic-auth";
 
 const app = express();
+
+function setUnauthorizedResponse(response) {
+  response.set("WWW-Authenticate", "Basic realm=Authorization Required");
+  response.sendStatus(401);
+}
+
+app.use((request, response, next) => {
+  let user = basicAuth(request);
+
+  if(!user || !user.name || !user.pass) {
+    return setUnauthorizedResponse(response);
+  }
+
+  if(user.name === process.env.AUTH_USER && user.pass === process.env.AUTH_PASS) {
+    next();
+  }
+  else {
+    return setUnauthorizedResponse(response);
+  }
+});
 
 app.use("/assets", express.static("build"));
 
